5e5c4af03dffa9c73c3cdfce8e06ab147ba90e6f
Gabriel Faria Genaro <leonismogabriel@gmail.com>
2025-04-29 01:45:22 -0300

    Edit: changes on sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,9 +1,10 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox, Search, Settings, GalleryVerticalEnd } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
+  SidebarHeader,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
@@ -43,9 +44,26 @@ const items = [
 export function AppSidebar() {
   return (
     <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Documentation</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup className="h-full">
-          <SidebarGroupLabel>Instagram Analytics</SidebarGroupLabel>
+          {/*<SidebarGroupLabel>Instagram Analytics</SidebarGroupLabel>*/}
           <SidebarGroupContent className="h-full flex flex-col justify-between">
             <SidebarMenu>
               {items.filter((item) => item.title != "Settings")
